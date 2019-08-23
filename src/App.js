610512a37a0a0fee9ea80df5207ecd6c8052b4c3
hashirshoaeb <hashirshoaeb@gmail.com>
2019-08-23 23:44:30 +0500

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,36 +1,36 @@
-import React, { Component } from 'react';
-import Typist from 'react-typist';
-import './App.css';
-import Configs from './configurations.json';
+import React, { Component } from "react";
+import Typist from "react-typist";
+import "./App.css";
+import Configs from "./configurations.json";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       darkBackgroundModes: [
-        'day',
-        'terminal',
-        'torquoise',
-        'alizarin',
-        'amythyst',
-        'carrot',
-        'peterriver'
+        "day",
+        "terminal",
+        "torquoise",
+        "alizarin",
+        "amythyst",
+        "carrot",
+        "peterriver"
       ],
       lightBackgroundModes: [
-        'night',
-        'lightred',
-        'lightpurple',
-        'lightgreen',
-        'lightblue',
-        'lightyellow'
+        "night",
+        "lightred",
+        "lightpurple",
+        "lightgreen",
+        "lightblue",
+        "lightyellow"
       ],
-      backgroundType: Configs.backgroundType || 'plain',
-      appClass: Configs.plainBackgroundMode || 'daylight',
-      devIntro: Configs.devIntro || 'Lorem Ipsum',
+      backgroundType: Configs.backgroundType || "plain",
+      appClass: Configs.plainBackgroundMode || "daylight",
+      devIntro: Configs.devIntro || "Lorem Ipsum",
       devDesc:
         Configs.devDesc ||
-        'Aute veniam ut deserunt cillum irure pariatur Lorem dolore anim nostrud quis veniam elit culpa.',
-      backgroundMode: 'default',
+        "Aute veniam ut deserunt cillum irure pariatur Lorem dolore anim nostrud quis veniam elit culpa.",
+      backgroundMode: "default",
       backgroundIndex: 0,
       bgStyle: {},
       icons: Configs.icons || []
@@ -42,12 +42,12 @@ class App extends Component {
       return true;
     } else if (this.checkIfGradientTypeEnabled()) {
       this.setState({
-        appClass: 'gradient',
+        appClass: "gradient",
         bgStyle: this.prepareGradientStyleSheets()
       });
     } else if (this.checkIfImageTypeEnabled()) {
       this.setState({
-        appClass: 'full-bg-image',
+        appClass: "full-bg-image",
         bgStyle: this.prepareBackgroundImageStyle()
       });
     }
@@ -55,41 +55,41 @@ class App extends Component {
 
   checkIfNightModeEnabled = () => {
     return (
-      this.state.backgroundType === 'plain' &&
-      this.state.appClass === 'nightlight'
+      this.state.backgroundType === "plain" &&
+      this.state.appClass === "nightlight"
     );
   };
 
   checkIfDayModeEnabled = () => {
     return (
-      this.state.backgroundType === 'plain' &&
-      this.state.appClass === 'daylight'
+      this.state.backgroundType === "plain" &&
+      this.state.appClass === "daylight"
     );
   };
 
   checkIfGradientTypeEnabled = () => {
-    return this.state.backgroundType === 'gradient';
+    return this.state.backgroundType === "gradient";
   };
 
   checkIfPlainTypeEnabled = () => {
-    return this.state.backgroundType === 'plain';
+    return this.state.backgroundType === "plain";
   };
 
   checkIfImageTypeEnabled = () => {
-    return this.state.backgroundType === 'image';
+    return this.state.backgroundType === "image";
   };
 
   prepareGradientStyleSheets = () => {
     if (Configs.gradientColors) {
       return {
-        background: 'linear-gradient(-45deg, ' + Configs.gradientColors + ')',
-        backgroundSize: '400% 400%'
+        background: "linear-gradient(-45deg, " + Configs.gradientColors + ")",
+        backgroundSize: "400% 400%"
       };
     } else {
       return {
         background:
-          'linear-gradient(-45deg, #EE7752, #E73C7E, #23A6D5, #23D5AB)',
-        backgroundSize: '400% 400%'
+          "linear-gradient(-45deg, #EE7752, #E73C7E, #23A6D5, #23D5AB)",
+        backgroundSize: "400% 400%"
       };
     }
   };
@@ -101,13 +101,13 @@ class App extends Component {
           'url("' +
           Configs.backgroundImageUrl +
           '") no-repeat center center fixed',
-        backgroundSize: 'cover'
+        backgroundSize: "cover"
       };
     } else {
       return {
         background:
           'url("/images/sample-background.jpg") no-repeat center center fixed',
-        backgroundSize: 'cover'
+        backgroundSize: "cover"
       };
     }
   };
@@ -123,13 +123,13 @@ class App extends Component {
   changeThemeMode = e => {
     if (this.checkIfNightModeEnabled()) {
       this.setState({
-        appClass: 'daylight',
+        appClass: "daylight",
         backgroundIndex: 0,
         backgroundMode: this.state.darkBackgroundModes[0]
       });
     } else if (this.checkIfDayModeEnabled()) {
       this.setState({
-        appClass: 'nightlight',
+        appClass: "nightlight",
         backgroundIndex: 0,
         backgroundMode: this.state.lightBackgroundModes[0]
       });
@@ -167,28 +167,34 @@ class App extends Component {
 
   render() {
     const {
-      appClass, bgStyle, backgroundMode, devIntro, devDesc, icons
+      appClass,
+      bgStyle,
+      backgroundMode,
+      devIntro,
+      devDesc,
+      icons
     } = this.state;
 
     return (
-      <div className={ appClass } style={ bgStyle }>
+      <div className={appClass} style={bgStyle}>
         <div className="change-mode" onClick={this.changeThemeMode} />
         <div
-          className={ backgroundMode }
-          onClick={this.changeBackgroundBasedonMode}>
+          className={backgroundMode}
+          onClick={this.changeBackgroundBasedonMode}
+        >
           <main className="App-main">
-            <h1 className="intro">{ devIntro }</h1>
+            <h1 className="intro">{devIntro}</h1>
             <div className="tagline">
-              <Typist>{ devDesc }</Typist>
+              <Typist>{devDesc}</Typist>
             </div>
             <div className="icons-social">
               {icons.map(icon => (
                 <a
                   target="_blank"
                   rel="noopener noreferrer"
-                  href={ `${icon.url}` }
+                  href={`${icon.url}`}
                 >
-                  <i className={ `fab ${icon.image}` } />
+                  <i className={`fab ${icon.image}`} />
                 </a>
               ))}
             </div>
