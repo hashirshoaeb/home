a9bdd37323aa0b9fd3d1b7003188337bce925a0a
Cody Bennett <hi@codyb.co>
2020-03-10 10:56:12 -0500

    Fixed typo in serviceworker.

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -25,7 +25,7 @@ export function register(config) {
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
     if (publicUrl.origin !== window.location.origin) {
-      // Our service worker won"t work if PUBLIC_URL is on a different origin
+      // Our service worker won't work if PUBLIC_URL is on a different origin
       // from what our page is served on. This might happen if a CDN is used to
       // serve assets; see https://github.com/facebook/create-react-app/issues/2374
       return;
@@ -80,7 +80,7 @@ function registerValidSW(swUrl, config) {
               }
             } else {
               // At this point, everything has been precached.
-              // It"s the perfect time to display a
+              // It's the perfect time to display a
               // "Content is cached for offline use." message.
               console.log("Content is cached for offline use.");
 
