8fe0a93be845a542aae7743a6d06b702d62ddd6e
Cody Bennett <hi@codyb.co>
2020-03-10 05:10:18 -0500

    Tidied project to es6 code style.

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,38 +1,39 @@
-import React, { Component } from "react";
-import "../App.css";
-import Configs from "../configurations.json";
-import "bootstrap/dist/css/bootstrap.min.css";
-import axios from "axios";
-import "bootstrap/dist/js/bootstrap.bundle.min";
-import ProjectCard from "./ProjectCard"
+import React, { Component } from 'react';
+import '../App.css';
+import Configs from '../configurations.json';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import ProjectCard from './ProjectCard';
 
 class Project extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
-      heading: "Recent Projects",
-      projectsArray: []
+      heading: 'Recent Projects',
+      projectsArray: [],
     };
   }
   componentDidMount = () => {
     this.handleRequest();
   };
 
-  handleRequest = e => {
+  handleRequest = (e) => {
     axios
       .get(Configs.gitHubLink + Configs.gitHubUsername + Configs.gitHubQuerry)
       .then(response => {
         // handle success
         // console.log(response.data.slice(0, 4));
-        this.setState({
+        return this.setState({
           projectsArray: response.data.slice(0, 4)
         });
       })
-      .catch(function(error) {
+      .catch(error => {
         // handle error
-        console.log(error);
+        return console.log(error);
       })
-      .finally(function() {
+      .finally(() => {
         // always executed
       });
   };
@@ -43,15 +44,15 @@ class Project extends Component {
         id="divproject"
         className="jumbotron jumbotron-fluid bg-transparent m-0"
       >
-        <div className=" container container-fluid p-5">
+        <div className="container container-fluid p-5">
           <h1 className="display-4 pb-5">{this.state.heading}</h1>
-          <div className=" row">
+          <div className="row">
             {this.state.projectsArray.map(project => (
               <ProjectCard
                 key={project.id}
                 id={project.id}
                 value={project}
-              ></ProjectCard>
+              />
             ))}
           </div>
         </div>
