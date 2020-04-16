100ba6857f96cac52c4a34e9465913080855869a
hashirshoaeb <hashirshoaeb@gmail.com>
2020-04-16 22:22:08 +0500

    blogpost link added

--- src/components/blog/BlogPost.jsx
+++ src/components/blog/BlogPost.jsx
@@ -2,13 +2,18 @@ import React, { useEffect, useState } from "react";
 import bloglist from "../../editable-stuff/blog";
 const BlogPost = ({ match }) => {
   const { id } = match.params;
+  const post = bloglist[id];
 
   return (
     <div className="container-lg mt-5">
-      {bloglist[id] && <div>{bloglist[id].getBlog()}</div>}
-      {!bloglist[id] && (
-        <h1 className="display-1 text-center">404 - Page not found</h1>
+      {post && (
+        <div>
+          <h1 className="display-2 text-center">{post.title}</h1>
+          <img className="img-fluid mb-2" src={post.image} alt={post.title} />
+          {post.getBlog()}
+        </div>
       )}
+      {!post && <h1 className="display-1 text-center">404 - Page not found</h1>}
     </div>
   );
 };
