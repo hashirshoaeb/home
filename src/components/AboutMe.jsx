a433aba074ed4c1dcf1dfa98ab0c81397764e6a6
Kishan1029 <56168653+Kishan1029@users.noreply.github.com>
2020-04-22 15:54:32 -0400

    added small error check

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -5,74 +5,84 @@ import axios from "axios";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import Pdf from "../editable-stuff/resume.pdf";
 
+
 const AboutMe = () => {
-  const [heading] = useState("About me");
-  const [aboutDev] = useState(Configs.aboutDev);
-  const [instaProfilePic, setInstaProfilePic] = useState("");
-  const [showInsta, setShowInsta] = useState(Configs.showInstaProfilePic);
-  const [resumeURL] = useState(Pdf);
+	const [heading] = useState("About me");
+	const [aboutDev] = useState(Configs.aboutDev);
+	const [instaProfilePic, setInstaProfilePic] = useState("");
+	const [showInsta, setShowInsta] = useState(Configs.showInstaProfilePic);
+	const [resumeURL] = useState(Pdf);
 
-  useEffect(() => {
-    if (showInsta) {
-      handleRequest();
-    }
-  }, [showInsta]);
+	useEffect(() => {
+		if (showInsta) {
+		handleRequest();
+		}
+	}, [showInsta]);
 
-  const handleRequest = e => {
-    axios
-      .get(Configs.instaLink + Configs.instaUsername + Configs.instaQuerry)
-      .then(response => {
-        // handle success
-        // console.log(response.data.graphql);
-        return setInstaProfilePic(
-          response.data.graphql.user.profile_pic_url_hd
-        );
-      })
-      .catch(error => {
-        // handle error
-        setShowInsta(false);
-        return console.error(error.message);
-      })
-      .finally(() => {
-        // always executed
-      });
-  };
+	const handleRequest = e => {
+		axios
+		.get(Configs.instaLink + Configs.instaUsername + Configs.instaQuerry)
+		.then(response => {
+			// handle success
+			// console.log(response.data.graphql);
+			return setInstaProfilePic(
+			response.data.graphql.user.profile_pic_url_hd
+			);
+		})
+		.catch(error => {
+			// handle error
+			setShowInsta(false);
+			return console.error(error.message);
+		})
+		.finally(() => {
+			// always executed
+		});
+	};
 
-  return (
-    <div id="aboutme" className="jumbotron jumbotron-fluid m-0">
-      <div className="container container-fluid p-5">
-        <div className="row">
-          {showInsta && (
-            <div className="col-5 d-none d-lg-block align-self-center">
-              <img
-                className="border border-secondary rounded-circle"
-                src={instaProfilePic}
-                alt="profilepicture"
-              />
-            </div>
-          )}
-          <div className={`col-lg-${showInsta ? "7" : "12"}`}>
-            <h1 className="display-4 mb-5 text-center">{heading}</h1>
-            <p className="lead text-center">{aboutDev}</p>
-            {resumeURL && (
-              <p className="lead text-center">
-                <a
-                  className="btn btn-outline-dark btn-lg"
-                  href={Pdf}
-                  target="_blank"
-                  rel="noreferrer noopener"
-                  role="button"
-                  aria-label="Resume/CV"
-                >
-                  Resume
-                </a>
-              </p>
-            )}
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	//checks if pdf file exist
+	const tryRequire = (Pdf) => {
+		try {
+			return require(`${Pdf}`);
+		} catch (err) {
+			return null;
+		}
+	};
+
+	return (
+		<div id="aboutme" className="jumbotron jumbotron-fluid m-0">
+		<div className="container container-fluid p-5">
+			<div className="row">
+			{showInsta && (
+				<div className="col-5 d-none d-lg-block align-self-center">
+				<img
+					className="border border-secondary rounded-circle"
+					src={instaProfilePic}
+					alt="profilepicture"
+				/>
+				</div>
+			)}
+			<div className={`col-lg-${showInsta ? "7" : "12"}`}>
+				<h1 className="display-4 mb-5 text-center">{heading}</h1>
+				<p className="lead text-center">{aboutDev}</p>
+				{resumeURL &&  (
+				<p className="lead text-center">
+					<a
+					className="btn btn-outline-dark btn-lg"
+					href={Pdf}
+					target="_blank"
+					rel="noreferrer noopener"
+					role="button"
+					aria-label="Resume/CV"
+					>
+					Resume
+					</a>
+				</p>
+				)}
+			</div>
+			</div>
+		</div>
+		</div>
+	);
 };
 
 export default AboutMe;
