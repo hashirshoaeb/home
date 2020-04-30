8839ae7cc7cf3331188d7d6470f867b01f8c054e
hashirshoaeb <hashirshoaeb@gmail.com>
2020-04-30 23:59:44 +0500

    blog setup

--- src/editable-stuff/blog.jsx
+++ src/editable-stuff/blog.jsx
@@ -5,20 +5,36 @@ const blog = new BlogBuilder({
   title: "My First Blog",
   image: "img",
   description:
-    "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem, dolores? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia provident similique corporis rem ullam odio excepturi voluptas harum, architecto sit!",
+    "Description Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem, dolores? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia provident similique corporis rem ullam odio excepturi voluptas harum, architecto sit!",
 })
   .addHeading("Heading")
-  .addParagraph("this is paragraph")
-  .addParagraph("2nd paragraph");
+  .addParagraph(
+    "1st Paragraph Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur vero reiciendis quas, autem sit culpa? Quibusdam, cupiditate voluptate a non nulla aliquid enim doloremque ullam, facilis quisquam similique hic omnis!"
+  )
+  .addParagraph(
+    "2nd Paragraph Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur ad, temporibus quae obcaecati eum expedita pariatur aspernatur recusandae beatae iste soluta sunt blanditiis dolore ipsam quia laboriosam quas perspiciatis architecto?"
+  );
 
 const blog1 = new BlogBuilder({
   title: "My Second Blog",
   image: "img",
-  description: "description---",
+  description:
+    "Description Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure tempore aut neque consectetur voluptatem quam nihil, facere earum adipisci, animi beatae dicta maiores, ipsam delectus ab molestias dolorum aperiam? Sapiente?",
 })
   .addHeading("Heading")
-  .addParagraph("this is paragraph")
-  .addParagraph("2nd paragraph");
+  .addParagraph(
+    "1st Paragraph Aspernatur vero reiciendis quas, autem sit culpa? Quibusdam, cupiditate voluptate a non nulla aliquid enim doloremque ullam, facilis quisquam similique hic omnis!"
+  )
+  .addParagraph(
+    "2nd Paragraph Consequuntur ad, temporibus quae obcaecati eum expedita pariatur aspernatur recusandae beatae iste soluta sunt blanditiis dolore ipsam quia laboriosam quas perspiciatis architecto?"
+  )
+  .addHeading("New Heading")
+  .addParagraph(
+    "1st Paragraph Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur vero reiciendis quas, autem sit culpa? Quibusdam, cupiditate voluptate a non nulla aliquid enim doloremque ullam, facilis quisquam similique hic omnis!"
+  )
+  .addParagraph(
+    "2nd Paragraph Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur ad, temporibus quae obcaecati eum expedita pariatur aspernatur recusandae beatae iste soluta sunt blanditiis dolore ipsam quia laboriosam quas perspiciatis architecto?"
+  );
 
 bloglist.push(blog1);
 bloglist.push(blog);
