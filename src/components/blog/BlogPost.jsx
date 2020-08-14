c92493a36ff7d8fdfb2921506391008a7f22b0f5
pubuser <pubuser7@gmail.com>
2020-08-14 14:07:41 +0430

    Remove unused variables

--- src/components/blog/BlogPost.jsx
+++ src/components/blog/BlogPost.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import bloglist from "../../editable-stuff/blog";
 const BlogPost = ({ match }) => {
   const { id } = match.params;
