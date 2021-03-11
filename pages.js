96fd17383fc45e31cd7976ca12900344755f036b
Tyler Kim <70506321+tylertaewook@users.noreply.github.com>
2021-03-11 16:37:57 +0900

    Update pages.js

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/tylertaewook/tylertaewook.github.io.git";
+const repoURL = "https://github.com/tylertaewook/tylertaewook.github.io.git";;
 
 ghpages.publish(
   pathname,
