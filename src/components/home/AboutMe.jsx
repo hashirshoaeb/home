eab6c1832af19ffac9423abfb7eb5ac3400da8f3
Tyler Kim <70506321+tylertaewook@users.noreply.github.com>
2021-03-11 20:03:53 +0900

    Update AboutMe.jsx

--- src/components/home/AboutMe.jsx
+++ src/components/home/AboutMe.jsx
@@ -1,13 +1,35 @@
-
 import React from "react";
+import Pdf from "../../editable-stuff/resume.pdf";
 
-const AboutMe = ({ heading, message, link, imgSize, resume, contactInfo }) => {
+import axios from "axios";
+
+const pictureLinkRegex = new RegExp(
+  /[(http(s)?):(www.)?a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/
+);
+
+const AboutMe = ({ heading, message, link, imgSize, resume }) => {
   const [profilePicUrl, setProfilePicUrl] = React.useState("");
-  const [showPic] = React.useState(Boolean(link));
+  const [showPic, setShowPic] = React.useState(Boolean(link));
 
   React.useEffect(() => {
+    if (link && !pictureLinkRegex.test(link)) {
+      handleRequest();
+    } else {
       setProfilePicUrl(link);
+    }
   }, [link]);
+
+  const handleRequest = async () => {
+    const instaLink = "https://www.instagram.com/";
+    const instaQuery = "/?__a=1";
+    try {
+      const response = await axios.get(instaLink + link + instaQuery);
+      setProfilePicUrl(response.data.graphql.user.profile_pic_url_hd);
+    } catch (error) {
+      setShowPic(false);
+      console.error(error.message);
+    }
+  };
 
   return (
     <div id="aboutme" className="jumbotron jumbotron-fluid m-0">
@@ -28,29 +50,6 @@ const AboutMe = ({ heading, message, link, imgSize, resume, contactInfo }) => {
           <div className={`col-lg-${showPic ? "7" : "12"}`}>
             <h2 className="display-4 mb-5 text-center">{heading}</h2>
             <p className="lead text-center">{message}</p>
-            <hr/>
-            <div className="container">
-              <div className="row">
-                <span className="text-dark mr-4 col-md-5">
-                  <i className="far fa-envelope" />{": " + contactInfo.email}
-                </span>
-                <span className="text-dark mr-4 col-md-5">
-                  <i className="fas fa-location-arrow" />{": "}
-                  <a
-                    className="text-dark text-decoration-none"
-                    href={contactInfo.locationLink}
-                  >
-                    {contactInfo.location}
-                </a>
-              </span>
-              </div>
-              <div className="row">
-                <span className="text-dark mr-4 col-md-6">
-                  <i className="fas fa-pencil-alt" />{": " + contactInfo.occupation}
-                </span>
-              </div>
-            </div>
-            <hr/>
             {resume && (
               <p className="lead text-center">
                 <a
