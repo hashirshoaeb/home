04dc78d770a065f59fbedb27dcb989b837c4308e
Tyler Kim <70506321+tylertaewook@users.noreply.github.com>
2021-03-11 17:31:39 +0900

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,16 +12,15 @@ const Footer = (props) => {
         <a
           className="badge badge-dark"
           rel="noopener"
-          href="https://github.com/hashirshoaeb"
+          href="https://github.com/tylertaewook"
           aria-label="My GitHub"
         >
-          Hashir Shoaib
+          Tyler Kim
         </a>{" "}
         using <i className="fab fa-react" />
         <p>
           <small className="text-muted">
-            Project code is open source. Feel free to fork and make your own
-            version.
+            
           </small>
         </p>
       </Container>
