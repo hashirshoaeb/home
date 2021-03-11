2477e7df18749264bcecff29d9814108192feafc
Tyler Kim <70506321+tylertaewook@users.noreply.github.com>
2021-03-11 17:53:00 +0900

    Update config.js

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -126,7 +126,7 @@ const getInTouch = {
   show: true,
   heading: "Get In Touch",
   message:
-    "I'm currently looking for full-time Software Engineering or Machine Learning opportunities! If you know of any positions available, if you have any questions, or if you just want to say hi, please feel free to email me at",
+    "If you have any questions, or if you just want to say hi, please feel free to email me at",
   email: "tylertaewook@gmail.com",
 };
 
