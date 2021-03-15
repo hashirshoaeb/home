ca722d85ce2cd170d7a75349d58aa01355e7cf50
Tyler Kim <70506321+tylertaewook@users.noreply.github.com>
2021-03-15 16:52:10 +0900

    Update ExperienceCard.jsx

--- src/components/home/ExperienceCard.jsx
+++ src/components/home/ExperienceCard.jsx
@@ -2,22 +2,37 @@ import React from 'react';
 
 import { 
     Col,
+    Button,
     Card
  } from "react-bootstrap";
 const ExperienceCard = ({data}) => {
     return ( 
-        <Col lg="6">
-            <div className="pb-5 text-center">
-                <img className=" bg-white mb-3" src={data.companylogo} alt=""/>
-                <p className="lead">
-                    {data.role}
-                    <br/>
-                    {data.date}
-                </p>
-                
-            </div>
+        <Col lg="4">
+             <Card className="shadow-lg--hover shadow border-0 text-center rounded my-2">
+               <Card.Header>
+                    <h5>{data.company}</h5>
+                </Card.Header>
+                <Card.Body className="py-5">
+                    <Card.Img className=" bg-white rounded-circle mb-3 img-center img-fluid shadow-lg" src={data.companylogo} style={{ width: "100px" }} alt=""/>
+                    <Card.Title tag="h5">{data.role}</Card.Title>
+                    <Card.Subtitle>{data.date}</Card.Subtitle>
+                    <Card.Text className="my-3 text-left">
+                        {data.desc}
+                    </Card.Text>
+                    <ul className="text-left">
+                            {
+                                data.descBullets ? 
+                                data.descBullets.map((desc) => {
+                                    return <li key={desc}>{desc}</li>
+                                }) : null
+                            }
+                        </ul>
+                    <div>
+                    </div>
+                </Card.Body>
+            </Card> 
         </Col>
      );
 }
- 
-export default ExperienceCard;
+
+export default ExperienceCard; 
