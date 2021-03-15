f6533b12005b2f5e50609d828631816f6e47bbc8
Tyler Kim <70506321+tylertaewook@users.noreply.github.com>
2021-03-15 19:28:28 +0900

    Update config.js

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -7,7 +7,7 @@ const navBar = {
 const mainBody = {
   gradientColors: "#4484ce, #1ad7c0, #ff9b11, #9b59b6, #ff7f7f, #ecf0f1",
   firstName: "Tyler",
-  middleName: "(태욱)",
+  middleName: "\"태욱\"",
   lastName: "Kim",
   message: " Making cool things since 2015😎.",
   icons: [
