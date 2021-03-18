e894aa14ee5d11503607adae91fbdaea495663f6
Tyler Kim <70506321+tylertaewook@users.noreply.github.com>
2021-03-19 00:01:06 +0900

    Update config.js

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -164,7 +164,7 @@ const experiences = {
         'Instead of using standard histogram operation, we used RBF (Radial Basis Function) to perform a localized binning operation without binning constraints.',
       ],
     },
-    paper: require("../editable-stuff/sstp-paper.pdf")
+    paper: require("../editable-stuff/sstp-paper.pdf"),
   ]
 }
 
