74a0b9d921ec99d468d671707d9fa759a3fa1d2f
Tyler Kim <70506321+tylertaewook@users.noreply.github.com>
2021-03-18 23:55:16 +0900

    Update ExperienceCard.jsx

--- src/components/home/ExperienceCard.jsx
+++ src/components/home/ExperienceCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { 
     Col,
@@ -29,7 +30,7 @@ const ExperienceCard = ({data}) => {
                         </ul>
                     <div>
                     </div>
-                    <Button variant="outline-success" size="lg">
+                    <Button href="home/src/editable-stuff/resume.pdf" variant="outline-success" size="lg">
                       Paper
                     </Button>{' '}
                 </Card.Body>
