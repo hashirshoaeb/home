7316f8eb0dd4fa7262e3073670f2f15c04bba289
jp053505 <jp053505@cerner.net>
2021-06-06 23:14:54 -0500

    Adding tracking

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,9 @@
 FROM node:10.16.0-alpine
-#-alpine3.11
 
 # Create app directory
 WORKDIR /usr/src/app
 
 COPY package*.json ./
-
-# RUN apk add --update --no-cache python2 alpine-sdk
 
 COPY . .
 
--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Bump this version every time you need update containers
-TAG="0.1.3"
+TAG="0.1.4"
 
 # Build and promote the image to integration
 docker build --no-cache . -t jjp1023/home:$TAG && \
