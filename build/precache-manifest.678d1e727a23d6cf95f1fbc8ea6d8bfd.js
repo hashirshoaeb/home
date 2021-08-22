9b035df8dad41fd6376de1a85a7eb67afad42002
moustafa <mogarcia626@gmail.com>
2021-08-21 22:08:12 -0400

    add build files

---
+++ build/precache-manifest.678d1e727a23d6cf95f1fbc8ea6d8bfd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "909f2b327b7269a6dfa45a7315199313",
+    "url": "/moustafa-personal-site/index.html"
+  },
+  {
+    "revision": "edb81de17560b4937f23",
+    "url": "/moustafa-personal-site/static/css/main.0abaa039.chunk.css"
+  },
+  {
+    "revision": "64cfbb6155e54c68d8b8",
+    "url": "/moustafa-personal-site/static/js/2.6dfc12a4.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/moustafa-personal-site/static/js/2.6dfc12a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edb81de17560b4937f23",
+    "url": "/moustafa-personal-site/static/js/main.b8d62ce4.chunk.js"
+  },
+  {
+    "revision": "160e05a8ff9f9601b3dd",
+    "url": "/moustafa-personal-site/static/js/runtime-main.45dc7e3b.js"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/moustafa-personal-site/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "6392d98721de62b144fea8bd84c711eb",
+    "url": "/moustafa-personal-site/static/media/moustafagarcia.6392d987.jpg"
+  }
+]);
