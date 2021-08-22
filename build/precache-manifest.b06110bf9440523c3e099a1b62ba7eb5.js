82077fe115ff1e48156816518b65dacbd7733e02
moustafa <mogarcia626@gmail.com>
2021-08-21 23:02:23 -0400

    still

---
+++ build/precache-manifest.b06110bf9440523c3e099a1b62ba7eb5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7862cb561c455a6431f43b82f8fa9456",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "0665f9e99fbea8903901",
+    "url": "/home/static/css/main.0abaa039.chunk.css"
+  },
+  {
+    "revision": "ccb429da1d4a0bd2cfd4",
+    "url": "/home/static/js/2.8aec16b3.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/home/static/js/2.8aec16b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0665f9e99fbea8903901",
+    "url": "/home/static/js/main.ac79c407.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "6392d98721de62b144fea8bd84c711eb",
+    "url": "/home/static/media/moustafagarcia.6392d987.jpg"
+  },
+  {
+    "revision": "377e281016a6fc38f4fe439eae347c48",
+    "url": "/home/static/media/moustafagarcia_resume.377e2810.pdf"
+  }
+]);
