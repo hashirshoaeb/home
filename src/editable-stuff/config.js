6b49efcb14e8f20905eaba83fe793836ac0f7518
moustafa <mogarcia626@gmail.com>
2021-08-21 22:51:15 -0400

    build deploy

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -162,4 +162,6 @@ const experiences = {
 //   show: false,
 // };
 
-export { navBar, mainBody, about, repos, skills, leadership, getInTouch, experiences };
+// export { navBar, mainBody, about, repos, skills, leadership, getInTouch, experiences };
+
+export { navBar, mainBody, about, repos, skills, getInTouch, experiences };
