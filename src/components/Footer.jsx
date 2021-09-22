ae1f97b8059507bac4907b9dab0cc2de445e372c
Amaljith <amaljith64@gmail.com>
2021-09-22 09:08:54 +0530

    new branch added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,16 +8,7 @@ const Footer = (props) => {
     <footer style={bgStyle} className="mt-auto py-5 text-center ">
       <Container>
         {props.children}
-        <i className="fas fa-code" /> with <i className="fas fa-heart" /> by{" "}
-        <a
-          className="badge badge-dark"
-          rel="noopener"
-          href="https://github.com/hashirshoaeb"
-          aria-label="My GitHub"
-        >
-          Hashir Shoaib
-        </a>{" "}
-        using <i className="fab fa-react" />
+        
         <p>
           <small className="text-muted">
             Project code is open source. Feel free to fork and make your own
