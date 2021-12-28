8e93f573d970236a0d7cfcecc8984264b98ecf7e
willem <willem.vangerwen@thoughtworks.com>
2021-12-28 16:28:38 +0100

    Repo URL updated

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/fruitsapling/fruitsapling.github.io.git";
+const repoURL = "https://fruitsapling.github.io/home";
 
 ghpages.publish(
   pathname,
