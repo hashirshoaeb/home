3a3479ba4ba6988e80ee6f808ee1d704f5651fa1
Elliott Evans <evanselliott1@gmail.com>
2022-11-09 23:33:03 -0600

    adding custom stuff

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -6,31 +6,31 @@ const navBar = {
 // Main Body SECTION
 const mainBody = {
   gradientColors: "#4484ce, #1ad7c0, #ff9b11, #9b59b6, #ff7f7f, #ecf0f1",
-  firstName: "Hashir",
+  firstName: "Elliott",
   middleName: "",
-  lastName: "Shoaib",
-  message: " Passionate about changing the world with technology. ",
+  lastName: "Evans",
+  message: " data scientist | machine learning engineer ",
   icons: [
     {
       image: "fa-github",
-      url: "https://github.com/hashirshoaeb",
+      url: "https://github.com/elliottevans",
     },
-    {
-      image: "fa-facebook",
-      url: "https://www.facebook.com/hashirshoaeb",
-    },
-    {
-      image: "fa-instagram",
-      url: "https://www.instagram.com/hashirshoaeb/",
-    },
+//    {
+//      image: "fa-facebook",
+//      url: "https://www.facebook.com/hashirshoaeb",
+//    },
+//    {
+//      image: "fa-instagram",
+//      url: "https://www.instagram.com/hashirshoaeb/",
+//    },
     {
       image: "fa-linkedin",
-      url: "https://www.linkedin.com/in/hashirshoaeb/",
+      url: "https://www.linkedin.com/in/elliott-evans/",
     },
-    {
-      image: "fa-twitter",
-      url: "https://www.twitter.com/hashirshoaeb/",
-    },
+//    {
+//      image: "fa-twitter",
+//      url: "https://www.twitter.com/hashirshoaeb/",
+//    },
   ],
 };
 
@@ -49,12 +49,13 @@ const mainBody = {
 //         resume: "https://docs.google.com/document/d/13_PWdhThMr6roxb-UFiJj4YAFOj8e_bv3Vx9UHQdyBQ/edit?usp=sharing",
 
 const about = {
-  show: true,
+  show: false,
   heading: "About Me",
-  imageLink: require("../editable-stuff/hashirshoaeb.png"),
+//  imageLink: require("../editable-stuff/hashirshoaeb.png"),
+  imageLink: "",
   imageSize: 375,
   message:
-    "My name is Hashir Shoaib. I’m a graduate of 2020 from National University of Sciences and Technology at Islamabad with a degree in Computer Engineering. I'm most passionate about giving back to the community, and my goal is to pursue this passion within the field of software engineering. In my free time I like working on open source projects.",
+    "Principal data scientist at Capital One. Michigan and Northwestern grad. Lover of anything math, statistics, baseball, and data.",
   resume: "https://docs.google.com/document/d/13_PWdhThMr6roxb-UFiJj4YAFOj8e_bv3Vx9UHQdyBQ/edit?usp=sharing",
 };
 
@@ -64,7 +65,7 @@ const about = {
 // If you want to display specfic projects, add the repository names,
 //      i.e ["repository-1", "repo-2"]
 const repos = {
-  show: true,
+  show: false,
   heading: "Recent Projects",
   gitHubUsername: "hashirshoaeb", //i.e."johnDoe12Gh"
   reposLength: 4,
