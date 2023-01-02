947889b28ace299e3dfbdc806e7fcaf36c81f325
Amy Garcia <Amy.Garcia@Kub.org>
2023-01-02 18:26:47 -0500

    commit

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/hashirshoaeb/hashirshoaeb.github.io.git";
+const repoURL = "https://zalf14.github.io/AmyGarciaPM/";
 
 ghpages.publish(
   pathname,
