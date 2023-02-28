e4db285a9664b4f957e5c3ac5aa9eb324d793d72
Shawn Soneja <shawn.soneja85@gmail.com>
2023-02-27 19:48:40 -0500

    updated fav icon and verbiage tweaks

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/hashirshoaeb/hashirshoaeb.github.io.git";
+const repoURL = "https://github.com/shawn-don-soneja/shawn-don-soneja.github.io.git";
 
 ghpages.publish(
   pathname,
