07f44ad7bf187d5e75d237a91289d497544b6867
FedericoIglesias <igl.fede@gmail.com>
2023-07-13 18:53:05 +0200

    modifyColors

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -5,7 +5,8 @@ const navBar = {
 
 // Main Body SECTION
 const mainBody = {
-  gradientColors: "#4484ce, #1ad7c0, #ff9b11, #9b59b6, #ff7f7f, #ecf0f1",
+  // gradientColors: "#4484ce, #1ad7c0, #ff9b11, #9b59b6, #ff7f7f, #ecf0f1",
+  gradientColors: "#c20000, #e00000, #ff0000, #ff4040, #ff7676, #ffffff",
   firstName: "Federico",
   middleName: "",
   lastName: "Iglesias",
