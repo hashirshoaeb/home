2f14c6873ab88a6f6b6ed17ee60c2b75da3b0c74
anyaguuu <ag156@rice.edu>
2023-07-20 15:57:09 -0700

    favicon

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -5,14 +5,15 @@ const navBar = {
 
 // Main Body SECTION
 const mainBody = {
-  gradientColors: "#4484ce, #1ad7c0, #ff9b11, #9b59b6, #ff7f7f, #ecf0f1",
+  gradientColors: "#4484ce, #1ad7c0, #ff9b11, #9b59b6, #ff7f7f, #ecf0f1", // TODO: do i really want gradient?
   firstName: "Anya",
   middleName: "",
   lastName: "Gu",
   message: " Clever message ", // TODO: change
   icons: [
+    // TODO: figure this out
     {
-      image: "",
+      image: "fa-github",
       url: "https://github.com/anyaguuu",
     },
     {
