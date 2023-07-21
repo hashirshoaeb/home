70ad8623b195b0073ad7dba7b5fbb6ff6bbe216c
anyaguuu <ag156@rice.edu>
2023-07-21 15:44:52 -0700

    make images smaller

--- src/components/home/ExperienceCard.jsx
+++ src/components/home/ExperienceCard.jsx
@@ -8,7 +8,7 @@ const ExperienceCard = ({ data }) => {
   return (
     <Col lg="6">
       <div className="pb-5 text-center">
-        <img className=" bg-white mb-3" src={data.companylogo} alt="" />
+        <img className=" bg-white mb-3" src={data.companylogo} style={{maxWidth: '150px', maxHeight: '150px'}} alt="" />
         <p className="lead">
           {data.role}
           <br />
