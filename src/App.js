832c0c472740690fa3e7ad2c8517a447c6eabe48
Ulairi <36573811+RunWang123@users.noreply.github.com>
2024-03-25 22:02:08 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -79,7 +79,7 @@ const App = () => {
   const titleRef = React.useRef();
 
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL + "/"}>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       {navBar.show && <Navbar ref={titleRef} />}
       <Routes>
         <Route path="/" exact element={<Home ref={titleRef} />} />
